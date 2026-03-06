6819ffb2e781036c76744747a7d0e3ff723b46d0
jGYQrw gga <Kul.vova2009@yandex.ru>
2026-03-06 20:42:16 +0300

    create alembic

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 
+from pydantic import BaseModel
 
-app = FastAPI(title="Events Aggregator")
+
+class HealthResponse(BaseModel):
+    status: str
+
+
+app = FastAPI(title="Events Aggregator", responses_model={200: HealthResponse})
 
 
 # Старт проекта
 @app.get("/api/health")
-async def health() -> dict:
-    return {"status": "ok"}
+async def health() -> HealthResponse:
+    return HealthResponse(status="ok")
