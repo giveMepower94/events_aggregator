88eedc664efd7b702a2e36fa07901874ed85bcda
jGYQrw gga <Kul.vova2009@yandex.ru>
2026-03-06 07:48:40 +0300

    run health

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from events-aggregator!")
+from fastapi import Depends
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.events_agg.db.session import get_session
+
+from fastapi import FastAPI
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI(title="Events Aggregator")
+
+
+# Старт проекта
+@app.get("/api/health")
+async def health() -> dict:
+    return {"status": "ok"}
+
+
+@app.get("/api/db-check")
+async def db_check(session: AsyncSession = Depends(get_session)) -> dict:
+    result = await session.execute(text("SELECT 1"))
+    return {"db": result.scalar_one()}
