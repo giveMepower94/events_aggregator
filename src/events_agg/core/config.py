e85040ae77ffc650744a5b18981ca087463b8dcf
jGYQrw gga <Kul.vova2009@yandex.ru>
2026-03-09 22:25:54 +0300

    change config

--- src/events_agg/core/config.py
+++ src/events_agg/core/config.py
@@ -1,18 +1,20 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env",
                                       env_file_encoding="utf-8",
+                                      populate_by_name=True,
                                       extra="ignore")
 
     # Provider API
-    events_provider_base_url: str
+    events_provider_base_url: str = Field(alias="EVENTS_PROVIDER_BASE_URL")
 
-    events_provider_api_key: str
+    events_provider_api_key: str = Field(alias="EVENTS_PROVIDER_API_KEY")
 
     # DB
-    database_url: str
+    database_url: str = Field(alias="POSTGRES_CONNECTION_STRING")
 
     # Caching
     seats_cache_ttl_seconds: int = 30
